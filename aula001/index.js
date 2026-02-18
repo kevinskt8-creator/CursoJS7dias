996079b8502d250700ba8f78b172836a548cc8eb
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-18 20:26:23 -0300

    Object

--- aula001/index.js
+++ aula001/index.js
@@ -1,9 +1,8 @@
-console.log("Javascript Funcionando");
+console.log("Javascript Funcionando")
 
-
-let firstname = 'Carlos';
+let firstname = 'Carlos'
 let lastname = 'Kevin'
-console.log(firstname);
+console.log(firstname)
 console.log(lastname)
 
 let perfil = '04'
@@ -11,3 +10,14 @@ console.log(perfil)
 
 let cidade = 'BSB'
 console.log(cidade)
+
+// Tipos de variaveis primitvas
+
+let itemName = 'Pen' //String
+let itemPrice = 3 //Number
+let itemAvailable = true //Boolean
+let itemColor // Undefined
+itemName = null // Null: Utilizado para apagar valor de variavel
+
+
+console.log(typeof itemName)
---
+++ aula002/index.js
@@ -0,0 +1,28 @@
+
+
+// Tipos de variaveis de Referencia
+
+// objetcs 
+// Array
+// function
+
+let itemName = 'Pen' 
+let itemPrice = 3 
+let itemAvailable = true 
+let itemColor 
+
+
+//Object : Grupo de informações em uma coisa só
+
+let Pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: 'Red'
+}
+
+Pen.itemColor = 'blue'
+
+console.log(Pen.itemName)
+
+
