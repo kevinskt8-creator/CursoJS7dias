e017fcb6729f25e2ea830630f28c46aa80b381b6
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-18 20:54:15 -0300

    Functions

---
+++ aula004/index.js
@@ -0,0 +1,17 @@
+// Function : Executar uma tarefa com parametros e argumentos
+
+                    // Parametro
+function saleStatus(status , total)  {
+    console.log('Transaction ' + status + ' ! Total amount: $', total)
+}
+
+saleStatus('Approved', 30) // Argumeto
+
+// Função de cálculos : 
+
+function percentage10(price) {
+   return price - ( price * 10 /100)
+}
+
+console.log(percentage10(40))
+
