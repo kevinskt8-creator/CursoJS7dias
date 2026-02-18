778f8747530a1441b8ee6475411c845e0114a3bc
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-18 19:54:33 -0300

    Variaveis

---
+++ aula001/index.js
@@ -0,0 +1,13 @@
+console.log("Javascript Funcionando");
+
+
+let firstname = 'Carlos';
+let lastname = 'Kevin'
+console.log(firstname);
+console.log(lastname)
+
+let perfil = '04'
+console.log(perfil)
+
+let cidade = 'BSB'
+console.log(cidade)
