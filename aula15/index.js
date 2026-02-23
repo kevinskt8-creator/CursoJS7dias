9e77c545f2add42c92c27af7d868c6af371aca11
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-23 17:10:59 -0300

    revertendo arrays

---
+++ aula15/index.js
@@ -0,0 +1,37 @@
+// Transformando duas array em uma só
+
+let numbers = [5, 9, 7, 8];
+let letters = ["a", "b", "c"];
+
+all = numbers.concat(letters);
+half = all.slice(3, 5);
+
+console.log(all);
+console.log(half);
+
+
+// Join array
+
+let clientes = ['Andre', 'Marcela', 'Jose']
+
+console.log(clientes)
+
+let clientesJoin = clientes.join(', ') // Adc contéudo em cada item da array
+
+console.log(clientesJoin)
+
+// Revertendo a array
+
+let clients = ['Ana', 'Carlos', 'Bruno']
+ 
+clients.sort() // organiza por ordem alfabetica
+clients.reverse() // organiza por ordem reversa
+
+
+console.log(clients)
+
+
+
+
+
+
