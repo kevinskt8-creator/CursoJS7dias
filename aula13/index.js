975bab17fc3365032187c6a37dd66808502b34d8
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-23 16:35:59 -0300

    Removendo itens de uma array

--- aula13/index.js
+++ aula13/index.js
@@ -10,9 +10,22 @@ num.splice(3, 0, 4, 5, 6) // por onde comecar, quantidade que deve ser deletado,
 
 console.log(num)
 
-// localizando itens em uma array primitiva
+//localizando itens em uma array primitiva
 
 const num2 = [6, 7, 8, 9];
 
 console.log(num2.indexOf(8));
 console.log(num2.includes(6));
+
+
+ //Removendo itens da array
+
+const num3 = [5, 6, 7, 8]
+
+const final= num3.pop() // Remove o ultimo item da array
+
+const final2 = num3.shift() // remove primeiro item da array
+
+const final3 = num3.splice(0, 1) // seleciona a posição e depois escolhe qual  item vc quer excluir
+
+console.log(num3)
