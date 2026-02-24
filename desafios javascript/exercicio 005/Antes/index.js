47f49ae64b9a89a31c3a327f8d1522c05693d36c
kevinskt8-creator <kevinskt8@gmail.com>
2026-02-24 20:20:31 -0300

    exercicio 005

---
+++ desafios javascript/exercicio 005/Antes/index.js
@@ -0,0 +1,17 @@
+const colors = ["Red", "Blue", "Yellow", "Purple", "Green"];
+const btn = document.getElementById("btn");
+const color = document.querySelector('.color')
+
+//No click
+btn.addEventListener("click", function () {
+const randomColor = getRandomColor();
+  document.body.style.background = colors[randomColor];// Aplicando a cor ao bakcground
+  color.textContent = colors[randomColor]
+
+  
+});
+
+//Gerar os numeros randomicos
+function getRandomColor() {
+  return Math.floor(Math.random() * colors.length)
+}
